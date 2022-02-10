2944c8e17a26e6473fb7abb589fca37782518050
Brett Fleming <brettfleming015@gmail.com>
2022-02-10 12:00:17 -0600

    exported salaryData and filter funtions

--- salaryData.js
+++ salaryData.js
@@ -28,3 +28,6 @@ const salaryData = [
   const getDataByCompany = company => {
     return salaryData.filter(obj => obj.company === company);
   }
+
+  export default salaryData
+export {getRoles, getCompanies, getDataByRole, GetDataByCompany};
